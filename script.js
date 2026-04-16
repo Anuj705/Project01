fc2b33a0b02fe06c32c3b685b5bc6862ff522773
Anuj Tikkas <anujtikkas40@gmail.com>
2026-04-16 20:24:46 +0530

    Day 5 code

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-alert("hey")
+function homepageAnimation(){
+     gsap.set(".slidesm", {scale: 5})
+
+var tl = gsap.timeline({
+     scrollTrigger: {
+          trigger:".home",
+          start:"top top",
+          end: "bottom bottom",
+          scrub: 3
+     }
+})
+
+
+tl
+.to(".vdodiv",{
+     '--clip': "0%",
+     ease: Power2,  
+}, 'a') 
+.to(".slidesm", {
+     scale: 1,
+     ease: Power2,  
+}, 'a') 
+
+.to(".slidesm", {
+     scale: 1,
+     ease: Power2,  
+}, 'a') 
+
+.to(".lft",{
+     xPercent: -10,
+     stagger: .03,
+     ease: Power4
+},'b')
+
+.to(".rgt",{
+     xPercent: 10,
+     stagger: .03,
+     ease: Power4
+},'b')
+}
+
+
+function realpageAnimation(){
+gsap.to(".slide",{
+     scrollTrigger: {
+          trigger: ".real",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: 2
+     },
+     xPercent: -200,
+     ease: Power4
+});
+}
+
+document.querySelectoreAll(".listelem")
+.forEach(function(e){
+     el.addEventListener("mousemove", function(dets){
+        gsap.to(this.querySelector(".picture"),{opacity: 1});
+     })
+     el.addEventListener("mouseleave", function(dets){})
+
+
+})
+
+realpageAnimation();
+homepageAnimation();
